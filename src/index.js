6f3eae90ecb0fa4c6c5af1a8ccc66fa9c0e4a13b
shiva <kumarshiva0707@gmail.com>
2022-12-18 05:09:09 +0530

    added unique constraint and started city-repository

--- src/index.js
+++ src/index.js
@@ -2,8 +2,9 @@ const express = require("express");
 // require('dotenv').config() written in serverconfig file
 
 const bodyparser = require("body-parser");
+const { City } = require('./models/index');
 const { PORT } = require('./config/serverConfig') ;
-
+// const CityRepository = require('./repository/city-repository');
 
 const setupAndStartServer = async () => {
 
@@ -14,10 +15,17 @@ const setupAndStartServer = async () => {
     app.use(bodyparser.urlencoded({extended : true}));
     // const PORT = 3000;
     //now we are acceesing port var from env
-    app.listen(PORT, () => {
+    app.listen(PORT, async() => {
         console.log(`server started as ${PORT}`);
         // console.log(process)
         // console.log(process.env)
+        // await City.create({
+        //     name : "New Delhi",
+        // })
+        //promise base call
+
+        // const repo = new CityRepository(); //first create obj
+        // repo.createCity({ name : "New  Delhi"});
     })
 
 }
