0e76920840d62b580d33dca37622457e9f86fcbf
shiva <kumarshiva0707@gmail.com>
2022-12-18 11:30:26 +0530

    completed city repository

--- src/repository/city-repository.js
+++ src/repository/city-repository.js
@@ -1,25 +1,55 @@
 const { City } = require('../models/index');
 
 class CityRepository{
-    async createCity({ name }){
+    async createCity({ name }){//obj.name but we destructured it 
         try {
-            const city = await City.create({ name });
+            const city = await City.create({ 
+                name  // same as name : name, //it makes function name  key automatically
+             });
             return city;
         } catch(error){
+            console.log("something went wrong in the repository layer");
             throw{error};
         }
     }
+// ------------------------------------
     async deleteCity(cityId){
         try{
-            await City.destroy({
+            await City.destroy({ //you write query inside {} like object
                 where : {
                     id : cityId
                 }
-            })
+            });
         }catch(error){
+            console.log("something went wrong in the repository layer");
             throw{error};
         }
     }
+//------------------------------------------
+    async updateCity(cityId, data){
+        try{
+            const city = await City.update(data,{
+                where :{
+                    id : cityId
+                }
+            } );
+            return city;
+        } catch(error){
+            console.log("something went wrong in repository layer");
+            throw {error};
+        }
+    }
+// -----------------------------------------
+    async getCity(cityId){
+        try{
+            const city = await City.findByPk(cityId);
+            return city;
+        } catch(error){
+            console.log("something went wrong in the repository layer");
+            throw {error} ;
+        }
+    }
+
 }
 
 module.exports = CityRepository;
